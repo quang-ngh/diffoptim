1b99b39d0831ecf9b9d4cf8c3ff82432c1cd5ebe
quang-ngh <quangngcs@gmail.com>
2023-03-12 03:53:50 +0000

    update debug

---
+++ visualize.py
@@ -0,0 +1,122 @@
+from config import *
+from model import *
+import torch
+import matplotlib.pyplot as plt 
+from config import DEVICE, get_celeba
+from main import save_images
+
+path_config = PathConfig()
+diffusion_config = DiffusionConfig()
+vit_config = ViTConfig()
+betas = diffusion_config.betas
+alphas_cumprod = diffusion_config.alphas_cumprod
+one_minus_alpha_cumprod = 1. - alphas_cumprod
+
+def convert_plot(x):
+    return x.detach().cpu().numpy()
+
+def plot_noise_config():
+
+    plt.subplot(1,3,1)
+    plt.plot(convert_plot(betas))
+
+    plt.subplot(1,3,2)
+    plt.plot(convert_plot(alphas_cumprod))
+
+    plt.subplot(1,3,3)
+    plt.plot(convert_plot(one_minus_alpha_cumprod))
+    plt.savefig(path_config.FIGURES / 'linear.png')
+
+def sample(batch, dict_path):
+
+    #   Load model 
+    model = ViT(
+        image_size          = vit_config.image_size,
+        patch_size          = vit_config.patch_size,
+        num_classes         = vit_config.num_classes,
+        dim                 = vit_config.dim,
+        depth               = vit_config.depth,
+        heads               = vit_config.heads,
+        mlp_dim             = vit_config.mlp_dim,
+        pool                = vit_config.pool,
+        channels            = vit_config.channels,
+        dim_head            = vit_config.dim_heads,
+        dropout             = vit_config.dropout,
+        emb_dropout         = vit_config.emb_dropout
+    ).double().to(DEVICE)
+    model.load_state_dict(torch.load(dict_path))
+    model.eval()
+
+    lr = 2e-3
+    B = batch
+    C,H,W = 3, 64, 64
+    z = torch.randn(B, C, H, W, device = DEVICE).double()
+    z = torch.nn.parameter.Parameter(z, requires_grad = True)
+    logits = torch.zeros(B, device=DEVICE, requires_grad = False).double()
+    optimizer = torch.optim.AdamW([z], lr = lr)
+
+    loss_fn = torch.nn.BCEWithLogitsLoss(reduction = 'mean')
+
+    sampling_steps = 10000
+    for i in range(sampling_steps):
+        out = model(z)
+        out = torch.nn.Sigmoid()(out).squeeze()
+
+        loss = loss_fn(out, logits)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print(f'Loss = {loss.item()} -- Noise level = {out[0]}')
+
+    save_images(z, path_config.SAVE_IMGS / 'sample.png')
+def test_model(dict_path, dataloader):
+    
+    model = ViT(
+        image_size          = vit_config.image_size,
+        patch_size          = vit_config.patch_size,
+        num_classes         = vit_config.num_classes,
+        dim                 = vit_config.dim,
+        depth               = vit_config.depth,
+        heads               = vit_config.heads,
+        mlp_dim             = vit_config.mlp_dim,
+        pool                = vit_config.pool,
+        channels            = vit_config.channels,
+        dim_head            = vit_config.dim_heads,
+        dropout             = vit_config.dropout,
+        emb_dropout         = vit_config.emb_dropout
+    ).double().to(DEVICE)
+    model.load_state_dict(torch.load(dict_path))
+    model.eval()
+
+
+    batch = next(iter(dataloader))
+    images, _ = batch
+    images = images[:4].double().to(DEVICE)
+    B, C, H, W = images.shape
+
+    betas                               = diffusion_config.betas
+    alphas_cumprod                      = diffusion_config.alphas_cumprod
+    one_minus_alpha_cumprod             = 1. - diffusion_config.alphas_cumprod
+    sqrt_alphas_cumprod                 = diffusion_config.sqrt_alphas_cumprod
+    sqrt_one_minus_alphas_cumprod       = diffusion_config.sqrt_one_minus_alphas_cumprod
+    
+    #   Add noise respected to noise level
+    t = torch.randint(0, diffusion_config.T, (B,), device=DEVICE).long()
+    z = q_sample(images, t, sqrt_alphas_cumprod, \
+                sqrt_one_minus_alphas_cumprod).double()
+
+    save_images(z, path_config.SAVE_IMGS / "test_noisy.png") 
+
+    output = model(z)
+    output = torch.nn.Sigmoid()(output)
+    output = output.squeeze() 
+
+    for i in range(t.shape[0]):
+        print(f'Time step = {t[i]} -- Real noise level = {one_minus_alpha_cumprod[t[i]]} -- Predict = {output[i]}')
+if __name__ == '__main__':
+
+    sample(16, path_config.CHECKPOINTS / "cosine/config1/checkpoints_epochs_40001")
+
+    # dataset, dataloader = get_celeba(5, path_config.CELEBA_DIR, 0)
+    # test_model(path_config.CHECKPOINTS / "cosine/config1/checkpoints_epochs_40001", dataloader)
